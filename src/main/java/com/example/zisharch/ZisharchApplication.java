e2aee552575bd59cd435fb8dbc01f289705a2ede
taro <taro@example.com>
2022-12-18 19:29:41 +0900

    first commit

---
+++ src/main/java/com/example/zisharch/ZisharchApplication.java
@@ -0,0 +1,13 @@
+package com.example.zisharch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZisharchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZisharchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zisharch/ZisharchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zisharch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZisharchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
