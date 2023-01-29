19e9c8120ea34b6496b7a6b90716a2c33c60c203
taro <taro@example.com>
2023-01-29 19:15:00 +0900

    20230129 first

--- src/main/java/com/example/zisharch/controller/TopicsController.java
+++ src/main/java/com/example/zisharch/controller/TopicsController.java
@@ -51,6 +51,19 @@ import com.example.zisharch.form.CommentForm;
 import org.thymeleaf.context.Context;
 import com.example.zisharch.service.SendMailService;
 
+import java.io.BufferedInputStream;
+import org.apache.commons.io.IOUtils;
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+import org.apache.sanselan.ImageReadException;
+import org.apache.sanselan.Sanselan;
+import org.apache.sanselan.common.IImageMetadata;
+import org.apache.sanselan.formats.jpeg.JpegImageMetadata;
+import org.apache.sanselan.formats.tiff.TiffImageMetadata.GPSInfo;
+
 @Controller
 public class TopicsController {
 
@@ -172,7 +185,7 @@ public class TopicsController {
     @RequestMapping(value = "/topic", method = RequestMethod.POST)
     public String create(Principal principal, @Validated @ModelAttribute("form") TopicForm form, BindingResult result,
             Model model, @RequestParam MultipartFile image, RedirectAttributes redirAttrs, Locale locale)
-            throws IOException {
+            throws ImageProcessingException, IOException, ImageReadException {
         if (result.hasErrors()) {
             model.addAttribute("hasMessage", true);
             model.addAttribute("class", "alert-danger");
@@ -212,7 +225,7 @@ public class TopicsController {
         return "redirect:/topics";
     }
 
-    private File saveImageLocal(MultipartFile image, Topic entity) throws IOException {
+    private File saveImageLocal(MultipartFile image, Topic entity) throws IOException,ImageProcessingException, ImageReadException {
         File uploadDir = new File("/uploads");
         uploadDir.mkdir();
 
@@ -225,7 +238,61 @@ public class TopicsController {
         File destFile = new File(realPathToUploads, fileName);
         image.transferTo(destFile);
 
+        setGeoInfo(entity, destFile, image.getOriginalFilename());
+        
         return destFile;
     }
 
+    private void setGeoInfo(Topic entity, BufferedInputStream inputStream, String fileName)
+            throws ImageProcessingException, IOException, ImageReadException {
+        Metadata metadata = ImageMetadataReader.readMetadata(inputStream);
+        setGeoInfo(entity, metadata, inputStream, null, fileName);
+    }
+
+    private void setGeoInfo(Topic entity, File destFile, String fileName)
+            throws ImageProcessingException, IOException, ImageReadException {
+        Metadata metadata = ImageMetadataReader.readMetadata(destFile);
+        setGeoInfo(entity, metadata, null, destFile, fileName);
+    }
+
+    private void setGeoInfo(Topic entity, Metadata metadata, BufferedInputStream inputStream, File destFile,
+            String fileName) {
+        if (log.isDebugEnabled()) {
+            for (Directory directory : metadata.getDirectories()) {
+                for (Tag tag : directory.getTags()) {
+                    log.debug("{} {}", tag.toString(), tag.getTagType());
+                }
+            }
+        }
+
+        try {
+            IImageMetadata iMetadata = null;
+            if (inputStream != null) {
+                iMetadata = Sanselan.getMetadata(inputStream, fileName);
+                IOUtils.closeQuietly(inputStream);
+            }
+            if (destFile != null) {
+                iMetadata = Sanselan.getMetadata(destFile);
+            }
+            if (iMetadata != null) {
+                GPSInfo gpsInfo = null;
+                if (iMetadata instanceof JpegImageMetadata) {
+                    gpsInfo = ((JpegImageMetadata) iMetadata).getExif().getGPS();
+                    if (gpsInfo != null) {
+                        log.debug("latitude={}", gpsInfo.getLatitudeAsDegreesNorth());
+                        log.debug("longitude={}", gpsInfo.getLongitudeAsDegreesEast());
+                        entity.setLatitude(gpsInfo.getLatitudeAsDegreesNorth());
+                        entity.setLongitude(gpsInfo.getLongitudeAsDegreesEast());
+                    }
+                } else {
+                    List<?> items = iMetadata.getItems();
+                    for (Object item : items) {
+                        log.debug(item.toString());
+                    }
+                }
+            }
+        } catch (ImageReadException | IOException e) {
+            log.warn(e.getMessage(), e);
+        }
+    }
 }
